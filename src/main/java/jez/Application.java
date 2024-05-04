cd68980cf7ee250a27953e02f9b9f73b6009d3be
Antoine Hazebrouck <antoine.hazebrouck.etu@univ-lille.fr>
2024-05-04 11:03:16 +0200

    tests

--- src/main/java/jez/Application.java
+++ src/main/java/jez/Application.java
@@ -1,38 +1,33 @@
 package jez;
 
 import java.io.IOException;
+import jez.builders.DataType;
 
 public class Application
 {
 
 	public static void main(String[] args) throws IOException
 	{
-		// DataFrame dataset = DataFrame.fromCsv("src/test/resources/cities.csv", ",");
+		DataFrame dataFrame = DataFrame.fromCsv("src/test/resources/cities.csv")
+			.withHeaders()
+			.withDelimiter(",")
+			.with("LatD").as(DataType.INTEGER)
+			.with("LatM").as(DataType.INTEGER)
+			.with("LatS").as(DataType.INTEGER)
+			.with("NS").as(DataType.STRING)
+			.with("LonD").as(DataType.INTEGER)
+			.with("LonM").as(DataType.INTEGER)
+			.with("LonS").as(DataType.INTEGER)
+			.with("EW").as(DataType.STRING)
+			.with("City").as(DataType.STRING)
+			.with("State").as(DataType.STRING)
+			.read();
 
-		// String horizontal = dataset.columns()
-		// 		.stream()
-		// 		.map(col -> "-".repeat(col.length()))
-		// 		.reduce("", (acc, value) -> acc += value + "-");
-
-		// System.out.println(horizontal);
-		// for (String column : dataset.columns())
-		// {
-		// 	System.out.print("|");
-		// 	System.out.print(column);
-		// }
-		// System.out.println("|");
-		// System.out.println(horizontal);
-
-		// for (Row row : dataset.rows()) {
-		// 	for (String column : dataset.columns())
-		// 	{
-		// 		int diffSize = column.length() - row.get(column).length();
-		// 		System.out.print("|");
-		// 		System.out.print(row.get(column).trim());
-		// 	}
-		// 	System.out.println("|");
-		// }
-		// System.out.println(dataset);
+		System.out.println(dataFrame.columns());
+		System.out.println(dataFrame.select("State", "City").row(0));
+		System.out.println(dataFrame.size());
+		dataFrame = dataFrame.where(row -> row.get("State").<String>get().contains("OH"));
+		System.out.println(dataFrame.size());
 	}
 
 }
